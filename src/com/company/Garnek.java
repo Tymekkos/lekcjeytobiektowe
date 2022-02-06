63750e9c2830c03890e0208c8a48abae5df2906b
Tymekkos <tymek.koson@gmail.com>
2022-02-06 21:51:24 +0100

    06.02

---
+++ src/com/company/Garnek.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Garnek {
+    final int srednica;
+    final int wysokosc;
+    String kolor;
+
+    public Garnek(int srednica, int wysokosc){
+        this.srednica = srednica;
+        this.wysokosc = wysokosc;
+    }
+
+    public Garnek(int srednica, int wysokosc, String kolor){
+        this.srednica = srednica;
+        this.wysokosc = wysokosc;
+        this.kolor = kolor;
+    }
+
+    public String gotuj(){
+        return "gotowanie w trakcie";
+    }
+
+    public String gotuj(boolean czyDodalismySol){
+        if(czyDodalismySol) {
+            return "gotowanie z sola";
+        }else{
+            return "gotowanie bez soli";
+        }
+    }
+}
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Garnek garnek = new Garnek(5, 10);
+
+	    String wiadomosc = garnek.gotuj();
+        System.out.println(wiadomosc);
+    }
+}
