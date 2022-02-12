4e0b7c40b206d37ef744a79820185abfac25dae6
Tymekkos <tymek.koson@gmail.com>
2022-02-12 17:14:51 +0100

    12.02 mala zmiana

--- src/com/company/computer/Drive.java
+++ src/com/company/computer/Drive.java
@@ -6,6 +6,19 @@ import java.util.List;
 import java.util.Map;
 
 public class Drive {
+    private final String type;
     List<File> hddFiles = new ArrayList<>();
     Map<String, File> ssdFiles = new HashMap<>();
+
+    public Drive(String type){
+        this.type = type;
+    }
+
+    public void addFile(File file){
+         if(type.equals("HDD")) {
+             hddFiles.add(file);
+         }else if(type.equals("SSD")){
+             ssdFiles.put(file.toString(), file);
+         }
+    }
 }
